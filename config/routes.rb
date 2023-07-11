3897bf9a32443172db4102c3a1de79e44efb8b4f
ilya <ilyalebedev1@yandex.ru>
2023-07-11 10:15:48 +0400

    feat: homepage and fly io deploy

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@
 
 Rails.application.routes.draw do
   mount Motor::Admin => '/admin'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/pages/*page' => 'pages#show'
+  root 'pages#show', page: 'home'
 end
