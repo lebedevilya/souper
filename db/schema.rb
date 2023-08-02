6cbf647ec7417fb367420e7e20ed1993c477b593
ilya <ilyalebedev1@yandex.ru>
2023-08-02 20:57:31 +0400

    wip: menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_162243) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_01_193204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_162243) do
     t.bigint "menu_category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["menu_category_id"], name: "index_menu_items_on_menu_category_id"
   end
 
